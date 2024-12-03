43bdc9e8c3e71581789fd1b6d3956cf3490fdaa9
AsenTodor0v <mang0pa19@gmail.com>
2024-12-03 14:47:22 +0200

    add save meals

--- NutriPage/meals/urls.py
+++ NutriPage/meals/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from NutriPage.meals import views
 from NutriPage.meals.views import CreateMealsView, EditMealsView, UserMealsView, DetailMealView, DeleteMealsView
 
 urlpatterns = [
@@ -8,4 +9,7 @@ urlpatterns = [
     path('my-meals/', UserMealsView.as_view(), name='my-meals'),
     path('details-meal/<int:pk>/', DetailMealView.as_view(), name='details-meal'),
     path('delete-meal/<int:pk>/delete/', DeleteMealsView.as_view(), name='delete-meal'),
+    path('post/<int:pk>/save/', views.save_mealplan, name='save_mealplan'),
+    path('saved-mealplans/', views.saved_mealplans, name='saved_mealplans'),
+    path('post/<int:pk>/unsave/', views.unsave_mealplan, name='unsave_mealplan'),
 ]
